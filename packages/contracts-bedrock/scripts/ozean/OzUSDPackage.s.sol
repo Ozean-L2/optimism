830f16350cb6ed1bacbba335d46cf10f79f0678f
William <william.ridge.95@gmail.com>
2024-10-08 14:08:14 +1100

    fix: deploy delegatecall

--- packages/contracts-bedrock/scripts/ozean/OzUSDPackage.s.sol
+++ packages/contracts-bedrock/scripts/ozean/OzUSDPackage.s.sol
@@ -4,21 +4,23 @@ pragma solidity 0.8.15;
 import { Script } from "forge-std/Script.sol";
 import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import { OzUSD } from "src/L2/OzUSD.sol";
+import { WozUSD } from "src/L2/WozUSD.sol";
 
-contract OzUSDDeploy is Script {
-    OzUSD public implementation;
-    TransparentUpgradeableProxy public proxy;
-    address public admin = makeAddr("admin");
+contract OzUSDPackage is Script {
+    address public admin = 0xa2ef4A5fB028b4543700AC83e87a0B8b4572202e;
     uint256 public initialSharesAmount = 1e18;
 
-    function run() external broadcast {
+    function run() external broadcast() {
         /// Deploy implementation
-        implementation = new OzUSD();
+        OzUSD implementation = new OzUSD();
 
         /// Deploy Proxy
-        proxy = new TransparentUpgradeableProxy{ value: initialSharesAmount }(
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy{ value: initialSharesAmount }(
             address(implementation), admin, abi.encodeWithSignature("initialize(uint256)", initialSharesAmount)
         );
+
+        /// Deploy wozUSD
+        WozUSD wozUSD = new WozUSD(OzUSD(payable(proxy)));
     }
 
     modifier broadcast() {
